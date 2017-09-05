effa5bc84b0cbe146572f7e9161110859577763f
Isaiah Cruz <isaiahgcruz@gmail.com>
2017-09-05 16:23:32 +0800

    Add client side sorting and filtering functionality on the sample usage

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         {
           header: 'Name',
           name: 'name',
-          filter: 'name_filter',
+          filter: 'name',
         },
         {
           header: '<i>Age</i>',
@@ -17,22 +17,27 @@
           name: 'email',
           filter: {
             options: [
+              {
+                label: 'all emails',
+                value: '',
+              },
               '@gmail.com',
               '@hotmail.com',
               '@yahoo.com',
             ],
-            key: 'email_filter',
+            key: 'email',
           },
         },
         {
           header: 'Sex',
           name: 'sex',
           filter: [
-            {
-              label: 'Male',
-              value: 'Male',
-            },
+            'Male',
             'Female',
+            {
+              label: 'All Sex',
+              value: '',
+            },
           ],
         },
       ]"
@@ -44,6 +49,14 @@
     >
       <template scope="props" slot="age">
         <span @click="$emit('watever', props.row)"> {{ props.row.age }} years old </span>
+      </template>
+
+      <template scope="props" slot="age_filter">
+        <input
+          class="Vuelma-Datatable__input input"
+          type="number"
+          v-model="filterParams[(typeof props.column.filter === 'boolean') ? props.column.name : props.column.filter]"
+        >
       </template>
     </datatable>
   </div>
@@ -61,7 +74,6 @@ export default {
   },
   data() {
     return {
-      rows: JSON.parse('[{"name":"Coby Gerhold","email":"Alisha.Bailey53@hotmail.com","age":25,"sex":"Female"},{"name":"Rupert Ankunding","email":"Heather52@hotmail.com","age":17,"sex":"Female"},{"name":"Jordy Bednar","email":"Albin.Jacobson@gmail.com","age":27,"sex":"Male"},{"name":"Mr. Teagan Jenkins","email":"Ignacio.Towne@hotmail.com","age":23,"sex":"Female"},{"name":"Aracely Mueller","email":"Precious.Pagac44@yahoo.com","age":29,"sex":"Female"},{"name":"Eveline Steuber","email":"Fritz_Wehner70@yahoo.com","age":20,"sex":"Female"},{"name":"Hershel Donnelly","email":"Oma39@gmail.com","age":29,"sex":"Female"},{"name":"Heather Hoeger","email":"Issac_Feeney@yahoo.com","age":16,"sex":"Female"},{"name":"Warren Altenwerth","email":"Donald.Grant@gmail.com","age":18,"sex":"Female"},{"name":"Meggie Kulas","email":"Flossie4@yahoo.com","age":24,"sex":"Male"},{"name":"Athena Hilpert","email":"Dolores.Cummerata@hotmail.com","age":20,"sex":"Female"},{"name":"Fernando Veum","email":"Humberto_Murphy@hotmail.com","age":18,"sex":"Female"},{"name":"Madilyn Schmeler","email":"Steve73@yahoo.com","age":21,"sex":"Female"},{"name":"Darwin Hickle","email":"Baby_Ebert@yahoo.com","age":18,"sex":"Female"},{"name":"Jonathon Hand","email":"Shania92@yahoo.com","age":23,"sex":"Female"},{"name":"Jack Sawayn","email":"Eriberto.Hamill73@hotmail.com","age":16,"sex":"Male"},{"name":"Cory Hilll","email":"Reina57@yahoo.com","age":24,"sex":"Male"},{"name":"Marta Considine","email":"Jedidiah.Considine53@hotmail.com","age":21,"sex":"Male"},{"name":"Darron Douglas","email":"Vaughn_Romaguera@hotmail.com","age":21,"sex":"Female"},{"name":"Lempi DAmore","email":"Aida51@yahoo.com","age":15,"sex":"Male"},{"name":"Dr. Deondre Nicolas","email":"Coralie_Altenwerth58@hotmail.com","age":22,"sex":"Male"},{"name":"Dr. Noble King","email":"Ayden.Hodkiewicz@yahoo.com","age":24,"sex":"Female"},{"name":"Ashly Weissnat","email":"Nicole_Bode91@hotmail.com","age":23,"sex":"Male"},{"name":"Luigi Gaylord","email":"Tianna48@gmail.com","age":22,"sex":"Female"},{"name":"Mr. Jaclyn Hauck","email":"Jodie.Beier@hotmail.com","age":19,"sex":"Female"},{"name":"Ms. Elvis Ziemann","email":"Jeff.Collins@gmail.com","age":25,"sex":"Male"},{"name":"Merlin Wintheiser","email":"Brenda94@yahoo.com","age":23,"sex":"Female"},{"name":"Kenna Yundt","email":"Clementina.Goldner@hotmail.com","age":29,"sex":"Male"},{"name":"Kelton King","email":"Sarina67@gmail.com","age":27,"sex":"Female"},{"name":"Mr. Kody Rempel","email":"Cristian.Murphy78@gmail.com","age":30,"sex":"Male"},{"name":"Seamus Stanton","email":"Thurman95@yahoo.com","age":22,"sex":"Female"},{"name":"Dr. Anita Koelpin","email":"Raleigh_Mosciski46@yahoo.com","age":18,"sex":"Male"},{"name":"Alverta Rippin","email":"Christelle_Tillman@hotmail.com","age":21,"sex":"Female"},{"name":"Myles Ledner","email":"Willa67@hotmail.com","age":18,"sex":"Male"},{"name":"Mr. Donny Quitzon","email":"Novella.Hauck@hotmail.com","age":23,"sex":"Female"},{"name":"Mrs. Thea Deckow","email":"Antoinette_Volkman@yahoo.com","age":16,"sex":"Female"},{"name":"Felicita Borer","email":"Jacinto.Greenholt@yahoo.com","age":30,"sex":"Male"},{"name":"Willow Bechtelar","email":"Alphonso71@yahoo.com","age":28,"sex":"Female"},{"name":"Dayna Waters","email":"Octavia14@hotmail.com","age":20,"sex":"Female"},{"name":"Dr. Ottilie Kihn","email":"Lily.Walsh@yahoo.com","age":19,"sex":"Male"},{"name":"Ryley Erdman","email":"Furman_Zieme85@gmail.com","age":16,"sex":"Male"},{"name":"Colin Erdman","email":"Jayson.Hirthe99@yahoo.com","age":21,"sex":"Female"}]'),
       sort: '',
       sortAsc: `
         <span class="icon">
@@ -69,12 +81,44 @@ export default {
         </span>
       `,
       filterParams: {
-        name_filter: 'hey',
+        name: '',
         age: '',
-        email_filter: '@yahoo.com',
-        sex: '',
+        email: '',
+        sex: 'Male',
       },
     };
+  },
+  computed: {
+    rows() {
+      let rows = JSON.parse('[{"name":"Coby Gerhold","email":"Alisha.Bailey53@hotmail.com","age":25,"sex":"Female"},{"name":"Rupert Ankunding","email":"Heather52@hotmail.com","age":17,"sex":"Female"},{"name":"Jordy Bednar","email":"Albin.Jacobson@gmail.com","age":27,"sex":"Male"},{"name":"Mr. Teagan Jenkins","email":"Ignacio.Towne@hotmail.com","age":23,"sex":"Female"},{"name":"Aracely Mueller","email":"Precious.Pagac44@yahoo.com","age":29,"sex":"Female"},{"name":"Eveline Steuber","email":"Fritz_Wehner70@yahoo.com","age":20,"sex":"Female"},{"name":"Hershel Donnelly","email":"Oma39@gmail.com","age":29,"sex":"Female"},{"name":"Heather Hoeger","email":"Issac_Feeney@yahoo.com","age":16,"sex":"Female"},{"name":"Warren Altenwerth","email":"Donald.Grant@gmail.com","age":18,"sex":"Female"},{"name":"Meggie Kulas","email":"Flossie4@yahoo.com","age":24,"sex":"Male"},{"name":"Athena Hilpert","email":"Dolores.Cummerata@hotmail.com","age":20,"sex":"Female"},{"name":"Fernando Veum","email":"Humberto_Murphy@hotmail.com","age":18,"sex":"Female"},{"name":"Madilyn Schmeler","email":"Steve73@yahoo.com","age":21,"sex":"Female"},{"name":"Darwin Hickle","email":"Baby_Ebert@yahoo.com","age":18,"sex":"Female"},{"name":"Jonathon Hand","email":"Shania92@yahoo.com","age":23,"sex":"Female"},{"name":"Jack Sawayn","email":"Eriberto.Hamill73@hotmail.com","age":16,"sex":"Male"},{"name":"Cory Hilll","email":"Reina57@yahoo.com","age":24,"sex":"Male"},{"name":"Marta Considine","email":"Jedidiah.Considine53@hotmail.com","age":21,"sex":"Male"},{"name":"Darron Douglas","email":"Vaughn_Romaguera@hotmail.com","age":21,"sex":"Female"},{"name":"Lempi DAmore","email":"Aida51@yahoo.com","age":15,"sex":"Male"},{"name":"Dr. Deondre Nicolas","email":"Coralie_Altenwerth58@hotmail.com","age":22,"sex":"Male"},{"name":"Dr. Noble King","email":"Ayden.Hodkiewicz@yahoo.com","age":24,"sex":"Female"},{"name":"Ashly Weissnat","email":"Nicole_Bode91@hotmail.com","age":23,"sex":"Male"},{"name":"Luigi Gaylord","email":"Tianna48@gmail.com","age":22,"sex":"Female"},{"name":"Mr. Jaclyn Hauck","email":"Jodie.Beier@hotmail.com","age":19,"sex":"Female"},{"name":"Ms. Elvis Ziemann","email":"Jeff.Collins@gmail.com","age":25,"sex":"Male"},{"name":"Merlin Wintheiser","email":"Brenda94@yahoo.com","age":23,"sex":"Female"},{"name":"Kenna Yundt","email":"Clementina.Goldner@hotmail.com","age":29,"sex":"Male"},{"name":"Kelton King","email":"Sarina67@gmail.com","age":27,"sex":"Female"},{"name":"Mr. Kody Rempel","email":"Cristian.Murphy78@gmail.com","age":30,"sex":"Male"},{"name":"Seamus Stanton","email":"Thurman95@yahoo.com","age":22,"sex":"Female"},{"name":"Dr. Anita Koelpin","email":"Raleigh_Mosciski46@yahoo.com","age":18,"sex":"Male"},{"name":"Alverta Rippin","email":"Christelle_Tillman@hotmail.com","age":21,"sex":"Female"},{"name":"Myles Ledner","email":"Willa67@hotmail.com","age":18,"sex":"Male"},{"name":"Mr. Donny Quitzon","email":"Novella.Hauck@hotmail.com","age":23,"sex":"Female"},{"name":"Mrs. Thea Deckow","email":"Antoinette_Volkman@yahoo.com","age":16,"sex":"Female"},{"name":"Felicita Borer","email":"Jacinto.Greenholt@yahoo.com","age":30,"sex":"Male"},{"name":"Willow Bechtelar","email":"Alphonso71@yahoo.com","age":28,"sex":"Female"},{"name":"Dayna Waters","email":"Octavia14@hotmail.com","age":20,"sex":"Female"},{"name":"Dr. Ottilie Kihn","email":"Lily.Walsh@yahoo.com","age":19,"sex":"Male"},{"name":"Ryley Erdman","email":"Furman_Zieme85@gmail.com","age":16,"sex":"Male"},{"name":"Colin Erdman","email":"Jayson.Hirthe99@yahoo.com","age":21,"sex":"Female"}]');
+
+      Object.keys(this.filterParams).forEach((key) => {
+        rows = rows.filter(row => String(row[key]).includes(this.filterParams[key]));
+      });
+
+      const sort = (this.sort.charAt(this.sort.length - 1) === '-') ? this.sort.slice(0, -1) : this.sort;
+
+      rows.sort((rowA, rowB) => {
+        const a = String(rowA[sort]).toUpperCase();
+        const b = String(rowB[sort]).toUpperCase();
+
+        if (a < b) {
+          return -1;
+        }
+
+        if (a > b) {
+          return 1;
+        }
+
+        return 0;
+      });
+
+      if (this.sort.charAt(this.sort.length - 1) === '-') {
+        rows.reverse();
+      }
+
+      return rows;
+    },
   },
   methods: {
     updateSort(sort) {
